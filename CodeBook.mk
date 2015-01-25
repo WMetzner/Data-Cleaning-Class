95528a8ee8289adec4325b64065eb64fe15a7505
Wolf Metzner <WMMetzner@gmail.com>
2015-01-25 17:34:07 -0600

    Create CodeBook.mk

---
+++ CodeBook.mk
@@ -0,0 +1,91 @@
+CodeBook.mk
+===========
+Author:		Wolf Metzner
+Class:		Getting and Cleaning Data
+Date:		  01/25/2015
+
+This code book lists the variables in the data table dfData1, as created in response to step 5 of the class project.
+Note that all measurements below are averaged over the Ssubjects and Activities.
+
+Variable					  Description
+Subject_ID					numeric id of a person participating in the Human Activity Recognition project
+Activity					  plain text (with numeric indicator) of one of the six activities
+tBodyAcc_mean_X     This and the following variables are taking from the input data.
+tBodyAcc_mean_Y     Please consult the README.txt for Human Activity Recognition project data set
+tBodyAcc_mean_Z
+tBodyAcc_std_X
+tBodyAcc_std_Y
+tBodyAcc_std_Z
+tGravityAcc_mean_X
+tGravityAcc_mean_Y
+tGravityAcc_mean_Z
+tGravityAcc_std_X
+tGravityAcc_std_Y
+tGravityAcc_std_Z		
+tBodyAccJerk_mean_X
+tBodyAccJerk_mean_Y
+tBodyAccJerk_mean_Z
+tBodyAccJerk_std_X
+tBodyAccJerk_std_Y
+tBodyAccJerk_std_Z
+tBodyGyro_mean_X
+tBodyGyro_mean_Y
+tBodyGyro_mean_Z
+tBodyGyro_std_X
+tBodyGyro_std_Y
+tBodyGyro_std_Z
+tBodyGyroJerk_mean_X
+tBodyGyroJerk_mean_Y
+tBodyGyroJerk_mean_Z
+tBodyGyroJerk_std_X
+tBodyGyroJerk_std_Y
+tBodyGyroJerk_std_Z
+tBodyAccMag_mean
+tBodyAccMag_std
+tGravityAccMag_mean
+tGravityAccMag_std
+tBodyAccJerkMag_mean
+tBodyAccJerkMag_std
+tBodyGyroMag_mean
+tBodyGyroMag_std
+tBodyGyroJerkMag_mean
+tBodyGyroJerkMag_std
+fBodyAcc_mean_X
+fBodyAcc_mean_Y
+fBodyAcc_mean_Z
+fBodyAcc_std_X
+fBodyAcc_std_Y
+fBodyAcc_std_Z
+fBodyAcc_meanFreq_X
+fBodyAcc_meanFreq_Y
+fBodyAcc_meanFreq_Z
+fBodyAccJerk_mean_X
+fBodyAccJerk_mean_Y
+fBodyAccJerk_mean_Z
+fBodyAccJerk_std_X
+fBodyAccJerk_std_Y
+fBodyAccJerk_std_Z
+fBodyAccJerk_meanFreq_X
+fBodyAccJerk_meanFreq_Y
+fBodyAccJerk_meanFreq_Z
+fBodyGyro_mean_X
+fBodyGyro_mean_Y
+fBodyGyro_mean_Z
+fBodyGyro_std_X
+fBodyGyro_std_Y
+fBodyGyro_std_Z
+fBodyGyro_meanFreq_X
+fBodyGyro_meanFreq_Y
+fBodyGyro_meanFreq_Z
+fBodyAccMag_mean
+fBodyAccMag_std
+fBodyAccMag_meanFreq
+fBodyBodyAccJerkMag_mean
+fBodyBodyAccJerkMag_std
+fBodyBodyAccJerkMag_meanFreq
+fBodyBodyGyroMag_mean
+fBodyBodyGyroMag_std
+fBodyBodyGyroMag_meanFreq
+fBodyBodyGyroJerkMag_mean
+fBodyBodyGyroJerkMag_std
+fBodyBodyGyroJerkMag_meanFreq
